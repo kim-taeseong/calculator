33928ff6d54c9c078ca0a369be4981d6da6ad11a
Yoojian <soiseaubleu@gmail.com>
2024-03-11 15:21:08 +0900

    commit

--- calculator.py
+++ calculator.py
@@ -13,8 +13,24 @@ print("두 값의 합은?:" , result)
     # 뺄셈
     # 곱셈
     # 나눗셈
-    def divide(a, b):
-        try:
-            return a / b
-        except ZeroDivisionError:
-            print('나누는 숫자가 0인지 확인해주세요')
+def divide(a, b):
+      try:
+          return a / b
+      except ZeroDivisionError:
+          print('나누는 숫자가 0인지 확인해주세요')
+
+# test
+# subtraction
+def subtraction():
+      숫자1 = float(input("첫 번째 숫자를 입력하세요: "))
+      연산자 = input("연산자를 입력하세요 (-): ")
+      숫자2 = float(input("두 번째 숫자를 입력하세요: "))
+
+      if 연산자 == '-':
+          결과 = 숫자1 - 숫자2
+      else:
+          print("올바른 연산자를 입력해주세요.")
+          return
+
+      print("결과:", 결과)
+

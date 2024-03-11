bfd8ab4430e819a2490c52a25bec5485a15a151b
taeseong <dnfrkwhr2424@naver.com>
2024-03-11 17:17:39 +0900

    feat: main.py user input and print result

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from .calculator import Calculator
 
-x = float(input("첫 번째 값을 입력하세요: "))
-y = float(input("두 번째 값을 입력하세요: "))
+number1 = float(input('첫 번째 값을 입력하세요: '))
+number2 = float(input('두 번째 값을 입력하세요: '))
+operator = input('연산자를 입력하세요(+, -, *, /): ')
 
-cal = Calculator()
-result = cal.add_cal(x,y)
+cal = Calculator(number1, number2)
 
-print("두 값의 합은?:" , result)
+cal_plus = cal.add_cal()
+cal_minus = cal.subtraction()
+cal_multiply = cal.multiply()
+cal_divide = cal.divide()
+
+print("두 값의 합은?:" , cal_plus)
+print("두 값의 차는?:" , cal_minus)
+print("두 값의 곱은?:" , cal_multiply)
+print("두 값의 나눗셈은?:" , cal_divide)
